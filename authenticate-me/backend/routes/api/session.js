d92e946853fc7809ddd568cbc47bfefb9d8231b1
agabriele73 <agabrielejr@gmail.com>
2023-02-07 21:29:51 -0800

    add user logout backend endpoint

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -35,6 +35,49 @@ router.post(
   );
 
 
+// backend/routes/api/session.js
+// ...
+
+// Log in
+router.post(
+  '/',
+  async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user: user
+    });
+  }
+);
+
+// backend/routes/api/session.js
+// ...
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+  
+  // ...
+
+
+
 
 
 module.exports = router;
