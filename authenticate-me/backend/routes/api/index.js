4ed33115638f2b32978c7cd84f4eaed92cfe2e39
agabriele73 <agabrielejr@gmail.com>
2023-02-07 20:52:56 -0800

    phase 3 done

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,6 +1,9 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
+const { setTokenCookie, restoreUser } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
 
 
 // backend/routes/api/index.js
@@ -11,4 +14,23 @@ router.post('/test', function(req, res) {
   });
   
   // ...
+
+
+
+  // backend/routes/api/index.js
+// ...
+
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+router.use(restoreUser)
+
+// ...
 module.exports = router;
