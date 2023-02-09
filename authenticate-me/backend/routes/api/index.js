3972c76041a9fb61444ee98e89ee481ddf986913
agabriele73 <agabrielejr@gmail.com>
2023-02-08 20:25:08 -0800

    some changes

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -28,19 +28,7 @@ router.post('/test', function(req, res) {
 
 
 
-  // backend/routes/api/index.js
-// ...
 
-// GET /api/set-token-cookie
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      }
-    });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
-});
 router.use(restoreUser)
 
 // ...
