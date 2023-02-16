f2475ce332536cacd7202a099c866df83483ea2d
agabriele73 <agabrielejr@gmail.com>
2023-02-15 19:41:39 -0800

    associations changed

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,9 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Review.belongsTo(models.User, {foreignKey: 'userId'})
-      Review.belongsTo(models.Spot, {foreignKey: 'spotId'})
-      Review.hasMany(models.reviewImage, {foreignKey: 'reviewId'})
+      Review.belongsTo(models.User, {foreignKey: 'id'})
+      Review.belongsTo(models.Spot, {foreignKey: 'id'})
+      Review.hasMany(models.reviewImage, {foreignKey: 'id'})
     }
   }
   Review.init({
