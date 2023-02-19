677fb1bc44b9a426286aeeddf48f8924240fe601
agabriele73 <agabrielejr@gmail.com>
2023-02-19 01:24:58 -0800

    code cleaned up all routes up to bookings done

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,9 +7,13 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 
+
+
+
+
+// current user reviews
 router.get('/current', async (req, res, next) => {
     const userId = req.user.id
-    
     const currReviews = await Review.findAll({where: {userId: userId}, include: 
         [
             {model: Spot, attributes: ['id','ownerId', 'address', 'city', 'state', 'country', 'lat','lng', 'name', 'description', 'price'], include: [
@@ -22,7 +26,6 @@ router.get('/current', async (req, res, next) => {
             {model: ReviewImage, attributes: ['id', 'url']}
         ]
 })
-    
 const modifiedCurrReviews = currReviews.map((review) => {
     let spot = review.Spot
     const urlMod = spot.SpotImages.map(obj => {
@@ -42,29 +45,88 @@ const modifiedCurrReviews = currReviews.map((review) => {
 res.json({Reviews: modifiedCurrReviews});
 })
 
+// post an image to a review
+router.post('/:reviewId/images', async (req, res) => {
+    const userId = req.user.id
+    const reviewId = parseInt(req.params.reviewId, 10)
+    const { url } = req.body
+    const review = await Review.findByPk(reviewId)
+    if(userId !== review.userId) {
+        res.status(400).json({
+            message: 'user not authorized',
+            statusCode: 400
+        })
+    }
+    if(!review) {
+        res.status(404).json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        })
+    }
+    const newReviewImage = await ReviewImage.create({
+        reviewId,
+        url
+    })
+    const images = await ReviewImage.findAll({ where: { reviewId } });
+    if (images.length >= 10) {
+        res.status(400).json({
+            message: "Maximum number of images for this review was reached",
+            statusCode: 400,
+        });
+        return;
+    }
+    res.json({id: newReviewImage.id, url: newReviewImage.url})
+})
 
 
+
+//edit a review
+router.put('/:reviewId', async (req, res) => {
+    const { review, stars } = req.body
+    const userId = req.user.id
+    const reviewId = parseInt(req.params.reviewId)
+    const upReview = await Review.findByPk(req.params.reviewId)
+    if(!upReview) {
+        res.status(404).json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        })
+    }
+    if(userId !== upReview.userId) {
+        res.status(400).json({
+            message: 'user not authorized',
+            statusCode: 400
+        })
+
+    }
+    const updatedReview = await upReview.update({
+        review,
+        stars
+    })
+    res.json(updatedReview)
+})
+
+
+// delete a review
 router.delete('/:reviewId', async (req, res, next) => {
-    
     const review = await Review.findByPk(req.params.reviewId)
-
     if (!review) {
         res.status(404).json({
             message: "Review couldn't be found",
             statusCode: 404
         })
     }
-
-
-    await review.destroy(
-
-        res.status(200).json(
-            {
-                "message": "Successfully deleted",
-                "statusCode": 200
-            }
-        )
-    )
-
+    await review.destroy()
+    res.status(200).json({message: "Successfully deleted", statusCode: 200})
 })
+
+
+
+
+
+
+
+
+
+
 module.exports = router
