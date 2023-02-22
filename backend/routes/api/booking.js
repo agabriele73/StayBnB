bd3d326fc29aee5198f7dff91b98f51e470a5afd
agabriele73 <agabrielejr@gmail.com>
2023-02-21 18:20:27 -0800

    edit and delete a spot routes

--- backend/routes/api/booking.js
+++ backend/routes/api/booking.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Spot, Review, SpotImage, User, ReviewImage, Booking} = require('../../db/models');
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Sequelize, Op } = require('sequelize');
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { Sequelize, Op, DATE } = require('sequelize');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
@@ -41,7 +41,7 @@ res.json({ bookings: updatedBookings });
 })
 
 
-
+// edit a spot
 router.put('/:bookingId', async (req, res) => {
     const { startDate, endDate } = req.body;
     const bookingId = req.params.bookingId
@@ -51,38 +51,45 @@ router.put('/:bookingId', async (req, res) => {
 
     if(!booking) {
     res.status(404).json({
-        message: "Spot couldn't be found",
+        message: "Booking couldn't be found",
         statusCode: 404 
 
     }) 
     }
     if(booking.userId !== userId) {
-        res.status(403).json({
+       return res.status(403).json({
             message: "User not authorized",
             statusCode: 403
         })
     }
-    const bookings = await Booking.findAll({
+    if(new Date(endDate) < new Date(startDate)) {
+        return res.status(400).json({
+            message: "End date must be after start date",
+            statusCode: 400
+        }) 
+    }
+    if(new Date() > new Date(endDate)) {
+        return res.status(400).json({
+            message: "End date must be in the future",
+            statusCode: 400
+        })
+    }
+    const existingBooking = await Booking.findOne({
         where: {
-            id: {[Op.ne]: bookingId},
-            [Op.or]: [
-                {
-                    startDate: {[Op.lte]: startDate},
-                    endDate: {[Op.gte]: startDate}
-                },
-                {
-                    startDate: {[Op.lte]: endDate},
-                    endDate: {[Op.gte]: endDate}
-                },
-                {
-                    startDate: {[Op.gte]: startDate},
-                    endDate: {[Op.lte]: endDate}
-                }
-            ]
-        }
+            spotId: bookingId,
+            startDate: {
+                [Op.lte]: new Date(endDate),
+            },
+            endDate: {
+                [Op.gte]: new Date(startDate)
+            },
+            id: {
+                [Op.ne]: bookingId
+            },
+        },
     });
-    if(bookings.length) {
-        res.status(403).json(
+    if(existingBooking) {
+        return res.status(403).json(
             {
                 "message": "Sorry, this spot is already booked for the specified dates",
                 "statusCode": 403,
@@ -94,15 +101,53 @@ router.put('/:bookingId', async (req, res) => {
             
         )
     }
-    const updatedBooking = await Booking.update({
-       where:{ bookingId},
-        startDate,
-        endDate
+    const updatedBooking = await booking.update({
+        startDate: new Date(startDate),
+        endDate: new Date(endDate)
+        
     })
+    console.log(updatedBooking)
     res.json(updatedBooking)
 
 })
 
+
+//delete a booking
+
+router.delete('/:bookingId', async (req, res) => {
+    const bookingId = req.params.bookingId
+    const userId = req.user.id
+
+        const booking = await Booking.findByPk(bookingId, {include: Spot})
+        
+            if(!booking) {
+                return res.status(404).json({
+                    message: "Booking couldn't be found",
+                    statusCode: 404
+                })
+            }
+
+            if(booking.startDate <= new Date()) {
+                return res.status(403).json(
+                    {
+                        message: "Bookings that have been started can't be deleted",
+                        statusCode: 403
+                    })
+            }
+            if(booking.userId !== userId && booking.Spot.ownerId !== userId) {
+                return res.status(403).json({
+                    message: "User not authorized",
+                    statusCode: 403
+                })
+            }
+
+            await booking.destroy()
+
+            return res.status(200).json({
+                message: "Successfully deleted",
+                statusCode: 200
+            })
+})
 
 
 module.exports = router
