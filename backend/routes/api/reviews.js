04dc0bb3d6b3872af344077553eaea28b8d25ac7
agabriele73 <agabrielejr@gmail.com>
2023-02-21 20:55:09 -0800

    reviews error fixed

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -111,16 +111,17 @@ router.put('/:reviewId', async (req, res) => {
 router.delete('/:reviewId', async (req, res, next) => {
     const userId = req.user.id
     const review = await Review.findByPk(req.params.reviewId)
-    if(review.userId !== userId) {
-        res.status(403).json({
-            message: "User not authorized",
-            statusCode: 403
-        })
-    }
+    console.log(review)
     if (!review) {
-        res.status(404).json({
+       return res.status(404).json({
             message: "Review couldn't be found",
             statusCode: 404
+        })
+    }
+    if(review.userId !== userId) {
+        return res.status(403).json({
+            message: "User not authorized",
+            statusCode: 403
         })
     }
     await review.destroy()
