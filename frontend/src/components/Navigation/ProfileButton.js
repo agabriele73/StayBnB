e3b43cb4723e6ad3aa6f8c220f19096882dca018
agabriele73 <agabrielejr@gmail.com>
2023-03-25 14:34:03 -0700

    modals converted

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import * as sessionActions from "../../store/session";
+
+
+const ProfileButton = ({user}) => {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef()
+
+    useEffect(() => {
+        if (!showMenu) return
+
+        const closeMenu = (e) => {
+            if(!ulRef.current.contains(e.target)) {
+                setShowMenu(false)
+            }
+        }
+        document.addEventListener("click", closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const openMenu = () => {
+        if (showMenu) return
+        setShowMenu(true)
+    }
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    }
+
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+
+    return (
+        <>
+            <button onClick={openMenu}>
+                <i className="fa-solid fa-user fa-sm"></i>
+            </button>
+            <ul className={ulClassName} ref={ulRef}>
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                    <button onClick={logout}>Logout</button>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+
+export default ProfileButton;
