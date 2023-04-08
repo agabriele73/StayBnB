f8ffddf414c6753fada396a90e8cfde652692649
agabriele73 <agabrielejr@gmail.com>
2023-04-08 16:09:20 -0700

    css added to form

--- frontend/src/components/SpotForm/index.js
+++ frontend/src/components/SpotForm/index.js
@@ -49,10 +49,13 @@ function SpotForm() {
             history.push(`/spots/${spotId}`)
         }).catch(async (res) => {
                 const data = await res.json();
+                console.log('thids is the response---------------',data)
+                
                 if (data && data.errors) {
-                setErrors(...errors, data.errors);
+                setErrors([...errors, data.errors]);
+                console.log('this is the errors after setErrors', errors)
                 } else {
-                setErrors(...errors, 'Something went wrong');
+                setErrors([...errors, 'Something went wrong']);
              }
         })
 
@@ -84,32 +87,36 @@ function SpotForm() {
 
 
     <form className="spot-form" onSubmit={handleSubmit}>
-        {errors.map((error, idx) => (
-            <p key={idx} className="errors">{error}</p>
-        ))}
+
         <h1>Create a new Spot</h1>
 
         <div className="section-1">
             <h3>Where's your place located at?</h3>
             <h4>Guest will only get your exact address once they booked a reservation</h4>
             <label>
-                Country
-                <input type="text"  value={country} placeholder="Country" onChange={(e) => setCountry(e.target.value)} required/>
+                Country <p className="errors">{errors.country}</p>
+                <input type="text"  value={country} placeholder="Country" onChange={(e) => setCountry(e.target.value)} />
             </label>
             <br />
             <label>
-                Street Address
-                <input type="text"  value={address} placeholder="Address" onChange={(e) => setAddress(e.target.value)} required/>
+                Street Address <p className="errors">{errors.address}</p>
+                <input type="text"  value={address} placeholder="Address" onChange={(e) => setAddress(e.target.value)} />
             </label>
             <br />
-            <label>
-                City
-                <input type="text" value={city} placeholder="City" onChange={(e) => setCity(e.target.value)} required/>
-            </label>,
-            <label>
-                State
-                <input type="text"  value={state} placeholder="State"  onChange={(e) => setState(e.target.value)} required/>
-            </label>
+            <div className='city-state'>
+                <div className="city">
+                    <label>
+                        City <p className="errors">{errors.city}</p>
+                        <input type="text" value={city} placeholder="City" onChange={(e) => setCity(e.target.value)} />
+                    </label>,
+                </div>
+                <div className="state">
+                    <label>
+                        State <p className="errors">{errors.state}</p>
+                        <input type="text"  value={state} placeholder="State"  onChange={(e) => setState(e.target.value)} />
+                    </label>
+                </div>
+            </div>
         </div>
 
 
@@ -117,7 +124,8 @@ function SpotForm() {
         <div className="section-2">
             <h3>Describe your place to guests</h3>
             <h4>Mention the best featuires of your space, any special amenities like fast wifi or parking, and what you love about the neighborhood.</h4>
-            <textarea value={description} placeholder="Please Write at least 30 characters"   onChange={(e) => setDescription(e.target.value)} minLength='30'></textarea>
+            <textarea value={description} placeholder="Please Write at least 30 characters" rows='10'  onChange={(e) => setDescription(e.target.value)} ></textarea>
+            <p className="errors">{errors.description}</p>
             <br />
         </div>
 
@@ -126,6 +134,7 @@ function SpotForm() {
             <h3>Create a title for your spot</h3>
             <h4>Catch guests attention with a spot title that highlights what makes your place special.</h4>
             <input type="text"  value={name} placeholder="Name of your spot" onChange={(e) => setName(e.target.value)}  />
+            <p className="errors">{errors.name}</p>
             <br />
         </div>
 
@@ -133,9 +142,11 @@ function SpotForm() {
         <div className="section-4">
             <h3>Set a base price for your Spot</h3>
             <h4>Competitive pricing can help your listing stand out and rank higher in search results.</h4>
-            <label>
-                $ <input type="text" value={price} placeholder="Price per night (USD)" onChange={(e) => setPrice(e.target.value)} />
-            </label>
+
+            $ <input type="text" value={price} placeholder="Price per night (USD)" onChange={(e) => setPrice(e.target.value)} />
+            <p className="errors">{errors.price}</p>
+
+
             <br />
         </div>
 
@@ -144,6 +155,7 @@ function SpotForm() {
             <h3>Liven up your spot with photos</h3>
             <h4>Submit a link to at least one photo to publish your spot.</h4>
             <input type="text"  placeholder="Preview Image URL" value={previewImage.url} onChange={(e) => setPreviewImage({url: e.target.value, previewImg: true})} />
+                <p className="errors">{errors.previewImage}</p>
             <br />
             <input type="text"  placeholder="Image URL" value={image2.url}  onChange={(e) => setImage2({url: e.target.value, previewImg: false})}/>
             <br />
