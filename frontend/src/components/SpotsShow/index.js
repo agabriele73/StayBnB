fe2159d6bb18722b733a3d0ae140d6d6574f01c8
agabriele73 <agabrielejr@gmail.com>
2023-04-15 14:11:22 -0700

    deleted not being used

--- frontend/src/components/SpotsShow/index.js
+++ frontend/src/components/SpotsShow/index.js
@@ -67,7 +67,7 @@ function SpotsShow({isLoaded}) {
 
                         <div className="card-text">
                         <p>{spot.city}, {spot.state}</p>
-                        {isNaN(spot.avgRating) ? <div className="stars-container"><p className="spot-new">New</p></div> : <div className="stars-container"><p className='fa-solid fa-star'>{spot.avgRating}</p></div>}
+                        {isNaN(spot.avgRating) ? <div className="stars-container"><p className="fa-solid fa-star">New</p></div> : <div className="stars-container"><p className='fa-solid fa-star'>{spot.avgRating}</p></div>}
 
                         <p>${spot.price} night</p>
                         </div>
